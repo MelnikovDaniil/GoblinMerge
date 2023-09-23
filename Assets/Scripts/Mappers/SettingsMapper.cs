17fd82e476822132897191e294177167b0f876f6
daniil.melnikau <daniil_melnikau1@epam.com>
2023-09-23 16:57:18 +0300

    SoundSystem added

---
+++ Assets/Scripts/Mappers/SettingsMapper.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class SettingsMapper
+{
+    private const string MapperName = "Settings";
+
+    public static bool GetVolume()
+    {
+        return PlayerPrefs.GetInt(MapperName + "Volume", 1) == 1;
+    }
+
+    public static void SetVolume(bool value)
+    {
+        PlayerPrefs.SetInt(MapperName + "Volume", value ? 1 : 0);
+    }
+
+    public static bool GetVibration()
+    {
+        return PlayerPrefs.GetInt(MapperName + "Vibration", 1) == 1;
+    }
+
+    public static void SetVibration(bool value)
+    {
+        PlayerPrefs.SetInt(MapperName + "Vibration", value ? 1 : 0);
+    }
+
+    public static void SetPostProcess(bool value)
+    {
+        PlayerPrefs.SetInt(MapperName + "PostProcess", value ? 1 : 0);
+    }
+}
