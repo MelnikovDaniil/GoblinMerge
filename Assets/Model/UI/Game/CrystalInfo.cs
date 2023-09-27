4f79b47ea4554cc16ee5ea1f135d840daf871f83
daniil.melnikau <daniil_melnikau1@epam.com>
2023-09-28 01:27:40 +0300

    Goblin integrated

--- Assets/Model/UI/Game/CrystalInfo.cs
+++ Assets/Model/UI/Game/CrystalInfo.cs
@@ -5,25 +5,35 @@ using UnityEngine;
 public class CrystalInfo : MonoBehaviour
 {
     public bool isBusy;
+    public Canvas canvas;
 
     [SerializeField]
     private float animationTime;
     [SerializeField]
     private TextMeshProUGUI lable;
 
+    private Camera _camera;
     private Animator _animator;
+    private RectTransform _rectTransform;
+    private Vector3 _screenCenter;
 
     private void Awake()
     {
+        _camera = Camera.main;
         _animator = GetComponent<Animator>();
+        _rectTransform = GetComponent<RectTransform>();
+       _screenCenter = new Vector2(Screen.width, Screen.height) / 2f;
     }
 
-    public void ShowCrystalInfo(int count, Vector2 position)
+    public void ShowCrystalInfo(float count, Vector3 worldPosition)
     {
         StopAllCoroutines();
         isBusy = true;
         gameObject.SetActive(true);
-        transform.position = position;
+        var newPos = _camera.WorldToScreenPoint(worldPosition);
+        newPos -= _screenCenter;
+
+        _rectTransform.anchoredPosition = newPos;
         lable.text = count.ToString();
 
         _animator.SetFloat("speedMultiplier", 1.0f / animationTime);
