36ed6b6a1dc1c522df4f339ddc7eb2180d18d308
daniil.melnikau <daniil_melnikau1@epam.com>
2023-10-19 01:27:27 +0300

    Added animation to button

--- Assets/Model/UI/UnitShop/PurchaseButton.cs
+++ Assets/Model/UI/UnitShop/PurchaseButton.cs
@@ -9,4 +9,26 @@ public class PurchaseButton : MonoBehaviour
     public Button button;
     public TextMeshProUGUI costText;
     public float cost;
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Click()
+    {
+        _animator.Play("Click", 0, 0);
+    }
+
+    public void Disable()
+    {
+        _animator.Play("Disable", 0, 0);
+    }
+
+    public void Enable()
+    {
+        _animator.Play("Enable", 0, 0);
+    }
 }
